f22faa97150b261e40fe289c1e02dd8b3983b4fa
NIDHI RANA <115101767+NidhiRana2020@users.noreply.github.com>
2023-07-10 13:10:36 +0530

    Add files via upload

---
+++ Blog/logout.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+$_SESSION = array();
+session_destroy();
+//header("location: index.php?Sucessfully=logout");
+echo'  <script>
+        alert("Sucessfully logout ")
+        document.location.href = "index.php?Sucessfully=logout";
+      </script>';
+
+?>
